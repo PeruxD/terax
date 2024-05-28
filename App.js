5258a4c12934fad5770a1f58692d04fdd5bd7b54
PeruxD <rezakeaocrew@gmail.com>
2024-05-28 06:27:23 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [blockNumber, setBlockNumber] = useState('');
+  const [blockData, setBlockData] = useState(null);
+
+  const fetchBlockData = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3000/block/${blockNumber}`);
+      setBlockData(response.data);
+    } catch (error) {
+      console.error('Error fetching block data:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Blockchain Explorer</h1>
+      <input
+        type="text"
+        value={blockNumber}
+        onChange={(e) => setBlockNumber(e.target.value)}
+        placeholder="Enter block number"
+      />
+      <button onClick={fetchBlockData}>Fetch Block</button>
+      {blockData && (
+        <div>
+          <h2>Block {blockData.number}</h2>
+          <pre>{JSON.stringify(blockData, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
