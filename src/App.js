c8d3ce80bf27c00b4bda70d5f6f44fab9beb0e7c
PeruxD <rezakeaocrew@gmail.com>
2024-05-28 07:10:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   const fetchBlockData = async () => {
     try {
-      const response = await axios.get(`http://localhost:3000/block/${blockNumber}`);
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/block/${blockNumber}`);
       setBlockData(response.data);
     } catch (error) {
       console.error('Error fetching block data:', error);
